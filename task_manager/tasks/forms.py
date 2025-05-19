3a98e1f27d0e3c84f94309916696eaab943bf4d6
Ihar <kostyuchik22@gmail.com>
2025-05-19 09:32:49 +0300

    correcting tasks form

--- task_manager/tasks/forms.py
+++ task_manager/tasks/forms.py
@@ -2,18 +2,66 @@ from django import forms
 from .models import Task
 from django.utils.translation import gettext as _
 
+from task_manager.users.models import User
+from task_manager.statuses.models import Status
+from task_manager.labels.models import Label
+
+
+# class CreateTaskForm(forms.ModelForm):
+#     description = forms.CharField(
+#         widget=forms.Textarea(attrs={'placeholder': _('Описание')}),
+#         label=_('Описание')
+#     )
+#
+#     class Meta:
+#         model = Task
+#         fields = ['title', 'description', 'status', 'executor', 'label']
+#         widgets = {
+#             'status': forms.Select(),
+#             'executor': forms.Select(),
+#             'label': forms.SelectMultiple(),
+#         }
+
 
 class CreateTaskForm(forms.ModelForm):
-    description = forms.CharField(
-        widget=forms.Textarea(attrs={'placeholder': _('Описание')}),
-        label=_('Описание')
-    )
-
     class Meta:
         model = Task
         fields = ['title', 'description', 'status', 'executor', 'label']
-        widgets = {
-            'status': forms.Select(),
-            'executor': forms.Select(),
-            'label': forms.SelectMultiple(),
-        }
+
+        title = forms.CharField(
+            widget=forms.TextInput(
+                attrs={
+                    'autofocus': True,
+                    'class': 'form-control',
+                    'placeholder': _('Имя')
+                }
+            )
+        )
+        description = forms.CharField(
+            widget=forms.Textarea(
+                attrs={
+                    'class': 'form-control',
+                    'placeholder': _('Описание')
+                }
+            ),
+            label=_('Описание')
+        )
+
+        status = forms.ModelChoiceField(
+            queryset=Status.objects.all(),
+            label=_('Статус'),
+            widget=forms.Select(attrs={'class': 'form-control'})
+        )
+
+        executor = forms.ModelChoiceField(
+            queryset=User.objects.all(),
+            required=False,
+            label=_('Исполнитель'),
+            widget=forms.Select(attrs={'class': 'form-control'})
+        )
+
+        label = forms.ModelMultipleChoiceField(
+            queryset=Label.objects.all(),
+            required=False, label=_('Метки'),
+            widget=forms.SelectMultiple(attrs={'class': 'form-select', 'size': '5'})
+        )
