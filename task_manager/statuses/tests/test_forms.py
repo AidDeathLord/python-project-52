a030b9c598272a5bbda93f51f907ac6d2109ed5e
Ihar <kostyuchik22@gmail.com>
2025-05-20 17:53:40 +0300

    correcting tasks tests

--- task_manager/statuses/tests/test_forms.py
+++ task_manager/statuses/tests/test_forms.py
@@ -4,11 +4,11 @@ from task_manager.statuses.forms import CreateStatusForm
 
 class StatusCreateFormTest(TestCase):
     def test_label_form_valid(self):
-        form = CreateStatusForm(data={'title': 'test'})
+        form = CreateStatusForm(data={'name': 'test'})
 
         self.assertTrue(form.is_valid())
 
     def test_label_form_invalid(self):
-        form = CreateStatusForm(data={'title': ''})
+        form = CreateStatusForm(data={'name': ''})
 
         self.assertFalse(form.is_valid())
